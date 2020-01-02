78366a8e85d2bf094802eed39f4cb8d8713853d6
alex.toplijski <alex.toplijski@gmail.com>
2020-01-02 21:31:41 +0200

    Remove tests from the types module

--- libs/types/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'types',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/types',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
