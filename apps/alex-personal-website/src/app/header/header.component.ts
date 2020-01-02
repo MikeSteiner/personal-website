b12dd02cf1402162e7a11f51d24280d425c7ad15
alex.toplijski <alex.toplijski@gmail.com>
2020-01-02 21:48:53 +0200

    Introduce app header and top navigation.

---
+++ apps/alex-personal-website/src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'personal-website-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
