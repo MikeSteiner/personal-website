8d540238718995064003cfb490fe0af45a4df44f
alex.toplijski <alex.toplijski@gmail.com>
2020-01-07 22:20:02 +0200

    Working on the header and navigation

--- apps/alex-personal-website/src/app/app/app.component.ts
+++ apps/alex-personal-website/src/app/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment as env } from '../../environments/environment';
 
 @Component({
   selector: 'personal-website-root',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alex-personal-website';
+
+  isProd = env.production;
+  envName = env.envName;
+  version = env.versions.app;
+  year = new Date().getFullYear();
+  // logo = require('../../assets/logo.png');
+  logo = '../../assets/logo.png';
+  languages = ['en', 'de', 'sk', 'fr', 'es', 'pt-br', 'zh-cn', 'he'];
+  navigation = [
+    { link: 'about', label: 'nesch.menu.about' },
+    { link: 'feature-list', label: 'nesch.menu.features' },
+    { link: 'examples', label: 'nesch.menu.examples' }
+  ];
+  navigationSideMenu = [
+    ...this.navigation,
+    { link: 'settings', label: 'nesch.menu.settings' },
+    { link: 'admin', label: 'nesch.menu.admin' }
+  ];
+
+  isAuthenticated$: Observable<boolean>;
+  stickyHeader$: Observable<boolean>;
+  language$: Observable<string>;
+  theme$: Observable<string>;
 }
--- apps/alex-personal-website/src/environments/environment.ts
+++ apps/alex-personal-website/src/environments/environment.ts
@@ -2,8 +2,29 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// const packageJson = require('../../../../package.json');
+
 export const environment = {
-  production: false
+  appName: 'Angular Ngrx Material Starter',
+  envName: 'DEV',
+  production: false,
+  test: false,
+  i18nPrefix: '',
+  versions: {
+    app: 'version',
+    // app: packageJson.version,
+    // angular: packageJson.dependencies['@angular/core'],
+    // ngrx: packageJson.dependencies['@ngrx/store'],
+    // material: packageJson.dependencies['@angular/material'],
+    // bootstrap: packageJson.dependencies.bootstrap,
+    // rxjs: packageJson.dependencies.rxjs,
+    // ngxtranslate: packageJson.dependencies['@ngx-translate/core'],
+    // fontAwesome: packageJson.dependencies['@fortawesome/fontawesome-free'],
+    // angularCli: packageJson.devDependencies['@angular/cli'],
+    // typescript: packageJson.devDependencies['typescript'],
+    // cypress: packageJson.devDependencies['cypress']
+  },
+  baseApiUrl: 'http://localhost:9001/api'
 };
 
 /*
